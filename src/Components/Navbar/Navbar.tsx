9393a37580ed4f3bd250a7cb2e5cad48ba0cec48
Vaibhav <tyagivaibhav520@gmail.com>
2025-04-24 17:30:14 +0530

    Commit hai

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -8,7 +8,6 @@ const Navbar = () => (
     <ul className="navbar-links">
       <li><Link to="/">Home</Link></li>
       <li><Link to="/login">Login</Link></li>
-      <li><Link to="/about">About Us</Link></li>
     </ul>
   </nav>
 );
